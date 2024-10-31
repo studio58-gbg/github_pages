c4d53aa77c7ea53f475a165cf1dd6c32086bdee0
davidle90 <89838376+davidle90@users.noreply.github.com>
2024-10-31 16:12:47 +0100

    update routes to github_pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,16 @@ import { Route, Routes } from "react-router-dom"
 import MainLayout from "./views/layouts/MainLayout"
 import Start from "./views/pages/Start"
 import Contacts from "./views/pages/Contacts"
-import Documentation from "./views/pages/Docs"
+import Docs from "./views/pages/Docs"
 
 function App() {
 
   return (
     <Routes>
-      <Route path="/" element={<MainLayout />}>
+      <Route path="/github_pages/" element={<MainLayout />}>
         <Route index element={<Start />} />
-        <Route path="/dokumentation" element={<Documentation />} />
-        <Route path="/kontakter" element={<Contacts />} />
+        <Route path="/github_pages/dokumentation" element={<Docs />} />
+        <Route path="/github_pages/kontakter" element={<Contacts />} />
       </Route>
     </Routes>
   )
--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -12,22 +12,22 @@ const SiteHeader = () => {
   return (
     <div className="bg-gray-100 py-3">
       <div className="mx-auto py-4 gap-8 w-2/3 flex justify-between items-center">
-        <div className="text-3xl font-bold leading-none tracking-tight">Studio 58</div>
+        <div className="text-3xl font-bold leading-none tracking-tight"><Link to="/github_pages/">Studio 58</Link></div>
 
         {/* Desktop Navigation */}
         <div className="hidden sm:block">
           <ul className="flex gap-4 uppercase font-semibold">
-            <li className={`hover:text-blue-600 ${location.pathname === '/' ? 'text-blue-800' : ''}`}>
-              <Link to="/">Start</Link>
+            <li className={`hover:text-blue-600 ${location.pathname === '/github_pages/' ? 'text-blue-800' : ''}`}>
+              <Link to="/github_pages/">Start</Link>
             </li>
             <li className="hover:text-blue-600">
               <a href="https://portal.nabo.se/kundportal/login">Nabo Portalen</a>
             </li>
-            <li className={`hover:text-blue-600 ${location.pathname === '/dokumentation' ? 'text-blue-800' : ''}`}>
-              <Link to="/dokumentation">Dokumentation</Link>
+            <li className={`hover:text-blue-600 ${location.pathname === '/github_pages/dokumentation' ? 'text-blue-800' : ''}`}>
+              <Link to="/github_pages/dokumentation">Dokumentation</Link>
             </li>
-            <li className={`hover:text-blue-600 ${location.pathname === '/kontakter' ? 'text-blue-800' : ''}`}>
-              <Link to="/kontakter">Kontakter</Link>
+            <li className={`hover:text-blue-600 ${location.pathname === '/github_pages/kontakter' ? 'text-blue-800' : ''}`}>
+              <Link to="/github_pages/kontakter">Kontakter</Link>
             </li>
           </ul>
         </div>
@@ -68,16 +68,16 @@ const SiteHeader = () => {
           </div>
           <ul className="space-y-1 font-medium text-center uppercase">
             <li className={`hover:text-blue-600 ${location.pathname === '/' ? 'text-blue-800' : ''}`}>
-              <Link to="/" onClick={toggleSidebar}>Start</Link>
+              <Link to="/github_pages/" onClick={toggleSidebar}>Start</Link>
             </li>
             <li className="hover:text-blue-600">
               <a href="https://portal.nabo.se/kundportal/login">Nabo Portalen</a>
             </li>
             <li className={`hover:text-blue-600 ${location.pathname === '/dokumentation' ? 'text-blue-800' : ''}`}>
-              <Link to="/dokumentation" onClick={toggleSidebar}>Dokumentation</Link>
+              <Link to="/github_pages/dokumentation" onClick={toggleSidebar}>Dokumentation</Link>
             </li>
             <li className={`hover:text-blue-600 ${location.pathname === '/kontakter' ? 'text-blue-800' : ''}`}>
-              <Link to="/kontakter" onClick={toggleSidebar}>Kontakter</Link>
+              <Link to="/github_pages/kontakter" onClick={toggleSidebar}>Kontakter</Link>
             </li>
           </ul>
         </div>
