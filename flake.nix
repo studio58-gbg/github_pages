e2bbdb9d5aabae1cbd4f5880ed220bd730e44596
henning phan <henning.phan@devies.se>
2024-11-19 20:40:58 +0100

    add nix flake with devshell

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: let
+    pkgs = nixpkgs.legacyPackages.aarch64-darwin;
+    in{
+
+    devShells.aarch64-darwin.default = pkgs.mkShell {
+      buildInputs = [ pkgs.nodejs ];
+
+    };
+
+  };
+}
