f814cd91ed09c1aaa7d510fb9af69bb060368ecc
henning phan <henning.phan@devies.se>
2024-12-01 23:52:43 +0100

    Remove obsolete pages
    They have been replaced with links to google drive documents

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { Route, Routes } from "react-router-dom"
 import MainLayout from "./views/layouts/MainLayout"
 import Start from "./views/pages/Start"
-import Contacts from "./views/pages/Contacts"
-import Docs from "./views/pages/Docs"
 
 function App() {
 
@@ -10,8 +8,6 @@ function App() {
     <Routes>
       <Route path="/" element={<MainLayout />}>
         <Route index element={<Start />} />
-        <Route path="/dokumentation" element={<Docs />} />
-        <Route path="/kontakter" element={<Contacts />} />
       </Route>
     </Routes>
   )
